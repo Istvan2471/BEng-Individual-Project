603a8b50e03ffd04b9e1ca17f734b74583d6ebfd
Istvan Darok <id419@ic.ac.uk>
2023-05-07 09:53:57 +0100

    changed address

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL # required to send email notifcations
-#SBATCH --mail-user=<your_username> # required to send email notifcations - please replace <your_username> with your college login name or email address
+#SBATCH --mail-user=id419 # required to send email notifcations - please replace <your_username> with your college login name or email address
 export PATH=/vol/bitbucket/id419/projenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
