dc8e135827bcc9e1f62fa1eb5af9e1934a5a1250
Istvan Darok <id419@ic.ac.uk>
2023-05-10 17:22:13 +0100

    changed script

--- submit.sh
+++ submit.sh
@@ -5,7 +5,7 @@
 export PATH=/vol/bitbucket/id419/projenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
-python /vol/bitbucket/id419/projenv/individual-project/main.py
+xvfb-run -s "-screen 0 1400x900x24" python /vol/bitbucket/id419/projenv/individual-project/main.py
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
