ff9de4e32640e8b8c598e8940c65df20a78e9608
Istvan Darok <id419@ic.ac.uk>
2023-05-21 11:19:46 +0100

    changed cuda ver

--- submit.sh
+++ submit.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=id419 # required to send email notifcations - please replace <your_username> with your college login name or email address
 export PATH=/vol/bitbucket/id419/projenv/bin/:$PATH
 source activate
-source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
+source /vol/cuda/12.0-cudnn8.0.5.39/setup.sh
 xvfb-run -a -s "-screen 0 1400x900x24" python /vol/bitbucket/id419/projenv/individual-project/main.py
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
