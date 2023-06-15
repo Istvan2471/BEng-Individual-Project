05f5ac8de4cd38174f9ea0c722608907935ae6eb
Istvan Darok <id419@ic.ac.uk>
2023-06-15 17:57:56 +0100

    fix

--- main.py
+++ main.py
@@ -353,7 +353,9 @@ for episode in tqdm(range(metrics['episodes'][-1] + 1, args.episodes + 1), total
       for (imged_belief, imged_prior_state) in zip(imged_beliefs, imged_prior_states):
         decoded_state = observation_model.forward(imged_belief, imged_prior_state)
         agent_positions.append(torch.split(decoded_state, 2, dim=1)[0])
-        returns = estimate_fm_value(agent_positions, imged_reward)
+      
+      pos_tensor = torch.as_tensor(agent_positions)
+      returns = estimate_fm_value(pos_tensor, imged_reward)
     else:
       returns = lambda_return(imged_reward, value_pred, bootstrap=value_pred[-1], discount=args.discount, lambda_=args.disclam)
     actor_loss = -torch.mean(returns)
