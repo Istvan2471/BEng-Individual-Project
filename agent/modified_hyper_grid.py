09e73462997fceabe09d9b007cc4b9270736e928
Istvan Darok <id419@ic.ac.uk>
2023-06-16 13:34:36 +0100

    changed backward_step

--- agent/modified_hyper_grid.py
+++ agent/modified_hyper_grid.py
@@ -2,12 +2,13 @@ from gfn.containers.states import States
 from gfn.envs import HyperGrid
 from gfn.envs.env import TensorLong, TensorBool, correct_cast, NonValidActionsError
 from torch import clone, gather
+from copy import deepcopy
 
 class ModifiedHyperGrid(HyperGrid):
     def backward_step(self, states: States, actions: TensorLong) -> States:
         """Function that takes a batch of states and actions and returns a batch of next
         states and a boolean tensor indicating initial states in the new batch."""
-        new_states = states.clone()
+        new_states = self.make_States_class(deepcopy(states.states_tensor))
         valid_states: TensorBool = ~new_states.is_initial_state
         valid_actions = actions[valid_states]
 
